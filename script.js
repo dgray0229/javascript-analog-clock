091cf3e67c8297cf90f13e1b8210f4cd747eef46
Devin Michael Gray <devingray@Devins-MacBook-Pro.local>
2017-07-21 21:14:11 -0700

    fixed javascript logic and degree jumping issue, displays time every second

--- script.js
+++ script.js
@@ -2,16 +2,24 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
-let hrPosition = 20, minPosition = 130, secPosition = 267;
+    var date = new Date();
 
+    let hr = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
 
-var date = new Date();
+    let hrPosition = (hr*360/12 + (min*(360/60)/12)), minPosition = ((min*360/60) + (sec*(360/60)/60)), secPosition = sec*360/60;
 
-let hr = date.getHours();
-let min = date.getMinutes();
-let sec = date.getSeconds();
+function runTheClock() {
 
-HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
-MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
-SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+    secPosition += 6;
+    minPosition += (6/60);
+    hrPosition += (3/360);
 
+    HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+    MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+    SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+
+}
+
+var interval = setInterval(runTheClock, 1000);
